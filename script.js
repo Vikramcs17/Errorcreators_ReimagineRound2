5022db29c22e113bd102fe02cb793ac4cb837380
Sahilk004 <148696304+Sahilk004@users.noreply.github.com>
2024-07-19 01:13:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+});
+
+gsap.registerPlugin(ScrollTrigger);
+gsap
+.timeline({
+  scrollTrigger: {
+    trigger: '.wrapper',
+    start: 'top top',
+    end: '+=150%',
+    pin: true,
+    scrub: true,
+  }
+})
+.to('.img', {
+  scale: 2,
+  z: 350,
+  transformOrigin: 'center center',
+  ease: 'power1.inOut'
+})
+.to(
+  '.section.hero',
+  {
+    scale: 1.5,
+    transformOrigin: 'center center',
+    ease: 'power1.inOut'
+  },
+);
+
 
