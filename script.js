8f3c9da1294aaeb7e24ee0d60089c8f3e6d48b43
Sahilk004 <148696304+Sahilk004@users.noreply.github.com>
2024-07-20 13:36:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
+const beforeAfterSlider = document.querySelector('.before-after-slider')
+const imgBefore = document.querySelector('.img-before')
+const line = document.querySelector('.before-line')
+
+beforeAfterSlider.addEventListener('mousemove',e=>{
+  imgBefore.style.width =`${e.layerX}px`
+  line.style.left =`${e.layerX}px`
+  
+})
+
+
+
 
 
