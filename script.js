2c82ae07e2dba547a622b402380876b660a84945
Vikram <vikramgupta1704@gmail.com>
2024-07-20 18:51:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var swiper = new Swiper(".mySwiper", {
 
 });
 
-
 const beforeAfterSlider = document.querySelector('.before-after-slider')
 const imgBefore = document.querySelector('.img-before')
 const line = document.querySelector('.before-line')
@@ -19,6 +18,17 @@ beforeAfterSlider.addEventListener('mousemove',e=>{
   
 })
 
+const container = document.querySelector('.container1');
+const nav = document.querySelector('nav');
+var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
 
-
+container.addEventListener('click', () => {
+    if (nav.classList.contains('show')) {
+        nav.classList.remove('show');
+        container.classList.remove('hide');
+    } else {
+        nav.classList.add('show');
+        container.classList.add('hide');
+    }
+});
 
