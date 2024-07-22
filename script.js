9f59690006c59021e5698c119ccd0a5e2a78755c
Jay Joshi <128173320+jayyx3@users.noreply.github.com>
2024-07-22 21:05:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
 
 function changeVideo(name){
-    const bgVideoList = document.querySelectorAll('.bg-video')
-    const models = document.querySelectorAll('.model')
+  const bgVideoList = document.querySelectorAll('.bg-video')
+  const models = document.querySelectorAll('.model')
 
-    bgVideoList.forEach(video =>{
-        video.classList.remove('active')
-        if(video.classList.contains(name)){
-            video.classList.add('active')
-        }
-    })
+  bgVideoList.forEach(video =>{
+      video.classList.remove('active')
+      if(video.classList.contains(name)){
+          video.classList.add('active')
+      }
+  })
 
-    models.forEach(model =>{
-        model.classList.remove('active')
-        if(model.classList.contains(name)){
-            model.classList.add('active')
-        }
-    })
+  models.forEach(model =>{
+      model.classList.remove('active')
+      if(model.classList.contains(name)){
+          model.classList.add('active')
+      }
+  })
 }
 
 const beforeAfterSlider = document.querySelector('.before-after-slider')
@@ -31,9 +31,9 @@ const imgBefore = document.querySelector('.img-before')
 const line = document.querySelector('.before-line')
 
 beforeAfterSlider.addEventListener('mousemove',e=>{
-  imgBefore.style.width =`${e.layerX}px`
-  line.style.left =`${e.layerX}px`
-  
+imgBefore.style.width =`${e.layerX}px`
+line.style.left =`${e.layerX}px`
+
 })
 
 const container = document.querySelector('.container1');
@@ -41,29 +41,29 @@ const nav = document.querySelector('nav');
 var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
 
 container.addEventListener('click', () => {
-    if (nav.classList.contains('show')) {
-        nav.classList.remove('show');
-        container.classList.remove('hide');
-    } else {
-        nav.classList.add('show');
-        container.classList.add('hide');
-    }
+  if (nav.classList.contains('show')) {
+      nav.classList.remove('show');
+      container.classList.remove('hide');
+  } else {
+      nav.classList.add('show');
+      container.classList.add('hide');
+  }
 });
 
 const blobCursor = (() => {  
-  const cursor = document.querySelector('#cursorBlob');
-  const links = document.querySelectorAll('.nav__link');
-  const setCursorPos = (e) => {
-    const { pageX: posX, pageY: posY } = e;
-    cursor.style.top = `${posY - (cursor.offsetHeight / 2)}px`;
-    cursor.style.left = `${posX - (cursor.offsetWidth / 2)}px`;
-  };
-  document.addEventListener('mousemove', setCursorPos);
-  
-  const setCursorHover = () => cursor.style.transform = 'scale(2.5)';
-  const removeCursorHover = () => cursor.style.transform = '';
-  links.forEach(link => link.addEventListener('mouseover', setCursorHover));
-  links.forEach(link => link.addEventListener('mouseleave', removeCursorHover));  
+const cursor = document.querySelector('#cursorBlob');
+const links = document.querySelectorAll('.nav__link');
+const setCursorPos = (e) => {
+  const { pageX: posX, pageY: posY } = e;
+  cursor.style.top = `${posY - (cursor.offsetHeight / 2)}px`;
+  cursor.style.left = `${posX - (cursor.offsetWidth / 2)}px`;
+};
+document.addEventListener('mousemove', setCursorPos);
+
+const setCursorHover = () => cursor.style.transform = 'scale(2.5)';
+const removeCursorHover = () => cursor.style.transform = '';
+links.forEach(link => link.addEventListener('mouseover', setCursorHover));
+links.forEach(link => link.addEventListener('mouseleave', removeCursorHover));  
 })();  
 
 
@@ -73,16 +73,90 @@ const linkImages = document.querySelectorAll('.hidden-img');
 
 
 for(let i = 0; i < link.length; i++) {
-  link[i].addEventListener('mousemove', (e) => {
-    linkHoverReveal[i].style.opacity = 1;
-    linkHoverReveal[i].style.transform = `translate(-100%, -50% ) rotate(5deg)`;
-    linkImages[i].style.transform = 'scale(1, 1)';
-    linkHoverReveal[i].style.left = e.clientX + "px";
-  })
-  
-  link[i].addEventListener('mouseleave', (e) => {
-    linkHoverReveal[i].style.opacity = 0;
-    linkHoverReveal[i].style.transform = `translate(-50%, -50%) rotate(-5deg)`;
-    linkImages[i].style.transform = 'scale(0.8, 0.8)';
-  })
+link[i].addEventListener('mousemove', (e) => {
+  linkHoverReveal[i].style.opacity = 1;
+  linkHoverReveal[i].style.transform = `translate(-100%, -50% ) rotate(5deg)`;
+  linkImages[i].style.transform = 'scale(1, 1)';
+  linkHoverReveal[i].style.left = e.clientX + "px";
+})
+
+link[i].addEventListener('mouseleave', (e) => {
+  linkHoverReveal[i].style.opacity = 0;
+  linkHoverReveal[i].style.transform = `translate(-50%, -50%) rotate(-5deg)`;
+  linkImages[i].style.transform = 'scale(0.8, 0.8)';
+})
 }
+
+
+// Variables
+const el = document.querySelector(".title-marquee");
+
+// Variables ~ Widths
+let elWidth = el.offsetWidth;
+let windowWidth = window.innerWidth;
+
+// Variables ~ Mouse
+let mouseX = 0;
+let prevMouseX = 0;
+
+// Target: value we want to animate to
+let skewTarget = 0;
+let translateTarget = 0;
+
+// WithEasing: value we use to animate
+let skewWithEasing = 0;
+let translateWithEasing = 0;
+
+// EasingFactor: determines how quick the animation/interpolation goes
+let skewEasingFactor = 0.1;
+let translateEasingFactor = 0.05;
+
+// Events
+window.addEventListener("mousemove", handleMouseMove);
+window.addEventListener("resize", handleWindowResize);
+
+// Functions
+function handleMouseMove(e) {
+  mouseX = e.pageX;
+}
+
+function handleWindowResize(e) {
+  elWidth = el.offsetWidth;
+  windowWidth = window.innerWidth;
+}
+
+function lerp(start, end, factor) {
+  return (1 - factor) * start + factor * end;
+}
+
+function animateMe() {
+  // Get difference between current and previous mouse position
+  skewTarget = mouseX - prevMouseX;
+  prevMouseX = mouseX;
+
+  // Calc how much we need to translate our el
+  translateTarget = (elWidth - windowWidth) / windowWidth * mouseX * -1;
+
+  // Ease between start and target values (skew)
+  skewWithEasing = lerp(skewWithEasing, skewTarget, skewEasingFactor);
+
+  // Limit our skew to a range of 75 degrees so it doesn't "over-skew"
+  skewWithEasing = Math.min(Math.max(parseInt(skewWithEasing), -75), 75);
+
+  // Ease between start and target values (translate)
+  translateWithEasing = lerp(
+  translateWithEasing,
+  translateTarget,
+  translateEasingFactor);
+
+
+  el.style.transform = `
+    translateX(${translateWithEasing}px)
+    skewX(${skewWithEasing}deg)
+  `;
+
+  // RAF
+  window.requestAnimationFrame(animateMe);
+}
+
+window.requestAnimationFrame(animateMe);
